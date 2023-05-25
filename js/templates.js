e074056c80726b35003796e07dcb40cbbec5bc92
Fabianjab <fabian.jablonka@web.de>
2023-05-25 18:25:37 +0200

    fabians_changes_25.05

--- js/templates.js
+++ js/templates.js
@@ -1,18 +1,27 @@
 function renderAddContact() {
-    let render = `<div class="add_contact">
-        <div class="contact_design"><img class="logo" src="">Tasks are better with a team!<div class="design_line"></div></div>
+    let render = `
+    <div class="add_contact">
+        <div class="contact_design">
+             <img class="small_logo" src="img/small_logo_add_contact.svg"> 
+             <span class="slogan_add_contact">Add contact</span> 
+             <span class="description_contact">Tasks are better with a team!</span>
+             <div class="design_line">
+             </div>
+        </div>
         <div class="contact_input">
-            <div class="close"></div>
-            <img class="avatar" src="">
-            <form onsubmit="addContact()">
-                <input type="text" class="input_name" id="input_name" placeholder="Name" required>
-                <input type="text" class="input_email" id ="input_email" placeholder="Email" required>
-                <input type="text" class="input_phone" id ="input_phone" placeholder="Phone" required>
+            <img class="avatar" src="img/icon_profile.svg">
+            <div class="contact_second_side">
+                <div class="close"></div>
+                <form class="form_conctact" onsubmit="addContact()">
+                    <input type="text" class="input_name" id="input_name" placeholder="Name" required>
+                    <input type="text" class="input_email" id ="input_email" placeholder="Email" required>
+                    <input type="text" class="input_phone" id ="input_phone" placeholder="Phone" required>
+                </form> 
                 <div class="form_buttons">
                     <button class="cancel">Cancel</button>
                     <button type="submit" class="submit">Create contact</button>
-                </div>
-            </form>
+                </div>    
+            <div>
         </div>
     </div>`;
     return render;
