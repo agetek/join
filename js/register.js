b886117d186dbfe40c830c007677428c030a5bce
katharinahamburg <katharina@hinnrichs.de>
2023-05-26 14:06:05 +0200

    regiter.js added

---
+++ js/register.js
@@ -0,0 +1,29 @@
+let users = [];
+
+async function init(){
+
+        loadUsers();
+}
+
+
+async function loadUsers(){
+    users = await getItem('users');
+}
+
+async function register(){
+    registerBtn.disabled = true;
+    users.push({
+        email: email.value,
+        password: password.value,
+    });
+
+    await setItem('users', JSON.stringify(users));
+
+    resetForm();
+}
+
+function resetForm(){
+    email.value = '';
+    password.value = '';
+    registerBtn.disabled = false;
+}
