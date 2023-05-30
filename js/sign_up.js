3a57d58a8052271858ebf81ea9448cbdb5a1f86e
katharinahamburg <katharina@hinnrichs.de>
2023-05-30 13:02:19 +0200

    some icons added

--- js/sign_up.js
+++ js/sign_up.js
@@ -31,8 +31,8 @@ function renderSignUpForm(){
     <div class="sign_up_head">
     <div class="sign_up_title">
     <img onclick="back_to_login" class="arrow_back_blue" src="./img/blue_arrow_back.svg">
-    <div class="headline_signup">Sign up</div>
-    <img class="underline_blue" src="./img/underline_blue">
+    <img class="logo_signup" src="./img/signup.svg">
+    <img class="underline_blue" src="./img/underline_blue.svg">
     </div>
     </div>
     <form onsubmit="register(); return false;">
