56189a83ac875648382ba93447c170ca2c0bacc2
Fabianjab <fabian.jablonka@web.de>
2023-05-30 12:49:29 +0200

    complete_add_contact_css

--- js/templates.js
+++ js/templates.js
@@ -2,25 +2,26 @@ function renderAddContact() {
     let render = `
     <div class="add_contact">
         <div class="contact_design">
-             <img class="small_logo" src="img/small_logo_add_contact.svg"> 
+                 <img class="small_logo" src="img/small_logo_add_contact.svg"> 
              <span class="slogan_add_contact">Add contact</span> 
              <span class="description_contact">Tasks are better with a team!</span>
              <div class="design_line">
              </div>
         </div>
         <div class="contact_input">
-            <img class="avatar" src="img/icon_profile.svg">
+                 <img class="avatar" src="img/icon_profile.svg">
             <div class="contact_second_side">
-                <div class="close"></div>
-                <form class="form_conctact" onsubmit="addContact()">
+                <div class="close-cross">
+                    <img src="img/cancel_cross.svg" class="close">
+                </div>
+                <form class="form_contact" onsubmit="addContact()">
                     <input type="text" class="input_name" id="input_name" placeholder="Name" required>
                     <input type="text" class="input_email" id ="input_email" placeholder="Email" required>
                     <input type="text" class="input_phone" id ="input_phone" placeholder="Phone" required>
                 </form> 
                 <div class="form_buttons">
                     <button class="cancel">Cancel</button>
-                    <button type="submit" class="submit">Create contact</button>
-                </div>    
+                    <button type="submit" class="submit">Create contact</button>   
             <div>
         </div>
     </div>`;
