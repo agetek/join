5a79321dcd10312798405d59d66afa9d77d7a8f4
katharinahamburg <katharina.hinnrichs@gmx.net>
2023-06-21 08:17:07 +0200

    comments deleted in storage.js

--- js/storage.js
+++ js/storage.js
@@ -1,20 +1,3 @@
-// async function setItem(key, value) {
-//     const payload = { key, value, token: STORAGE_TOKEN };
-//     return fetch(STORAGE_URL, { method: 'POST', body: JSON.stringify(payload) })
-//         .then(res => res.json());
-// }
-
-// async function getItem(key) {
-//     const url = `${STORAGE_URL}?key=${key}&token=${STORAGE_TOKEN}`;
-//     return fetch(url).then(res => res.json()).then(res => {
-//         // Verbesserter code
-//         if (res.data) { 
-//             return res.data.value;
-//         } throw `Could not find data with key "${key}".`;
-//     });
-// }
-
-
 function login() {
     let userEmail = document.getElementById('email').value;
     let userPassword = document.getElementById('password').value;
